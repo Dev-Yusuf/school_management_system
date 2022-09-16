5bc512d1c7b2ce23c288f4f79a0dad468afaef33
Dev-Yusuf <devyusuf01@gmail.com>
2022-09-16 08:52:48 -0700

    added static files

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -149,6 +149,9 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 JAZZMIN_SETTINGS = JAZZMIN_SETTINGS
 
